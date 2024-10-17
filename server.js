088f343a2a2fe2de993c63a2d45f5a1a5f0588b8
nathanzin22 <nathanfusinato@outlook.com>
2024-10-17 09:50:26 -0400

    Update server.js to use port 10000 instead of 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 10000;
 const path = require('path');
 const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
