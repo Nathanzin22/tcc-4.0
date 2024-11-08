c4f01992072d5fdbe8d10fd0a828bfbec4a673cb
Nathanzin22 <nathanfusinato@outlook.com>
2024-11-07 20:40:30 -0400

    Ajuste

--- db/dbconexão.js
+++ db/dbconexão.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Configurações de conexão com o banco de dados
 const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
+  user: 'nathanzin',
+  host: 'dpg-cs8h8p88fa8c73btt5hg-a',
   database: 'tcc_nathan',
-  password: 'postgres',
+  password: 'xU9uGN6CQCyeBLXDGA2VWvAbkp2SPnaC',
   port: 5432, // porta padrão do PostgreSQL
 });
 
